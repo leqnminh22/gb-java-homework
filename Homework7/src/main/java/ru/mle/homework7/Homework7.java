df1fe2854315dddfb4de10782eda32d4cd949cb0
leqnminh <leqnminh22@gmail.com>
2022-02-02 17:14:56 +0300

    edited homework6

---
+++ Homework7/src/main/java/ru/mle/homework7/Homework7.java
@@ -0,0 +1,16 @@
+package ru.mle.homework7;
+
+import java.util.Locale;
+
+public class Homework7 {
+
+    public static void main(String[] args) {
+
+        int n = 200;
+        String str = String.valueOf(n);
+
+        System.out.println(str);
+
+
+    }
+}
