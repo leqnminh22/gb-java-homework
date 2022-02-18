5e7cdc4947c6585460e14bb0900afdfd3075b0c5
leqnminh <leqnminh22@gmail.com>
2022-02-18 21:32:23 +0300

    homework12 Homework 12 for check

---
+++ homework12/src/main/java/ru/mle/homework12/Homework12.java
@@ -0,0 +1,62 @@
+package ru.mle.homework12;
+
+public class Homework12 {
+
+    public static final int SIZE = 10000000;
+    public static final int HALF = SIZE / 2;
+
+    public float[] calculate(float[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+        return arr;
+    }
+
+    public void threadOne() {
+        float[] arr = new float[SIZE];
+
+        for (int i = 0; i < SIZE; i++)
+            arr[i] = 1.0f;
+            long a = System.currentTimeMillis();
+            calculate(arr);
+            System.out.println("Время завершения с одним потоком: " + (System.currentTimeMillis() - a));
+
+    }
+
+    public void threadTwo() {
+        float[] arr = new float[SIZE];
+        float[] arr1 = new float[HALF];
+        float[] arr2 = new float[HALF];
+
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = 1.0f;
+
+        long a = System.currentTimeMillis();
+        System.arraycopy(arr, 0, arr1, 0, HALF);
+        System.arraycopy(arr, HALF, arr2, 0, HALF);
+
+
+        new Thread(() -> {
+                float[] a1 = calculate(arr1);
+                System.arraycopy(a1, 0, arr1, 0, a1.length);
+            }).start();
+
+        new Thread(() -> {
+            float[] a2 = calculate(arr2);
+            System.arraycopy(a2, 0, arr2, 0, a2.length);
+            }).start();
+
+            System.arraycopy(arr1, 0, arr, 0, HALF);
+            System.arraycopy(arr2, 0, arr, HALF, HALF);
+            System.out.println("Время завершения с двумя потоками: " + (System.currentTimeMillis() - a));
+
+    }
+
+    public static void main(String[] args) {
+
+        Homework12 a = new Homework12();
+        a.threadOne();
+        a.threadTwo();
+
+    }
+}
